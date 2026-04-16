e6330912a2c3406c48659d2d2ae9ea5103461381
bcastoldi <bcastoldi@frba.utn.edu.ar>
2026-04-15 21:59:31 -0300

    - Se agrego la carga dinamica de configuracion (IP, Puerto y Log Level). - Se implemento la conexion como cliente hacia el Kernel Scheduler. - Se integro el protocolo de handshake: envio de MSG_HANDSHAKE_IO y espera de confirmacion MSG_OK. - Se añadieron validaciones de argumentos de entrada y manejo de errores en sockets. - Se incluyo la limpieza de memoria (config_destroy, log_destroy y free).

--- io/src/io.c
+++ io/src/io.c
@@ -18,37 +18,76 @@
 #include "conexiones.h"
 #include "mensajes.h"
  
-t_config* config = config_create(argv[1]);
+t_config* config;
+t_log* logger;
 
-char* ip = config_get_string_value(config, "IP_KERNEL");
-char* puerto = config_get_string_value(config, "PUERTO_KERNEL");
-
-// 1. Primero leemos la palabra del archivo config
-char* nivel_de_log_texto = config_get_string_value(config, "LOG_LEVEL");
-// 2. La traducimos al "idioma" que entiende el Logger
-t_log_level nivel_traducido = log_level_from_string(nivel_de_log_texto);
-// 3. Usamos ese nivel traducido al crear el Logger
-logger = log_create("io.log", "IO", true, nivel_traducido);
-
-int fd_ks = crear_conexion(ip, puerto);
  
 int main(int argc, char* argv[]) {
  
-    // TODO Bianca: verificar argumentos (config + tipo)
+    // Verificar argumentos (config + tipo)
     // El tipo viene por parámetro: argv[2] → "STDIN", "STDOUT" o "SLEEP"
+
+    // (Si argc es menor a 2, es que no escribieron el nombre del archivo)
+    if (argc < 2) {
+        printf("Error: Falta el archivo de configuracion\n Uso: ./bin/io [archivo_config] [TIPO]\n TIPO: STDIN/STDOUT/SLEEP");
+        return EXIT_FAILURE;
+    }
  
-    // TODO Bianca: cargar config con config_create()
+    // Cargar config con config_create()
+    config = config_create(argv[1]);
+    // Verificar que exista
+    if (config == NULL) {
+        printf("Error: no se pudo abrir el archivo de configuracion: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
  
-    // TODO Bianca: crear logger con log_create()
+    // Crear logger 
+    // 1. Primero leemos la palabra del archivo config
+    char* nivel_de_log_texto = config_get_string_value(config, "LOG_LEVEL");
+    // 2. La traducimos al "idioma" que entiende el Logger
+    t_log_level nivel_traducido = log_level_from_string(nivel_de_log_texto);
+    // 3. Usamos ese nivel traducido al crear el Logger
+    logger = log_create("io.log", "IO", true, nivel_traducido);
+
+    // Conexión a Kernel Scheduler
+    // 1. Obtener el value de las variables del config
+    char* ip = config_get_string_value(config, "KS_IP");
+    char* puerto = config_get_string_value(config, "KS_PORT");
+
+    // 2. Conectarse 
+    int fd_ks = crear_conexion(ip, puerto); // int fd_ks = crear_conexion(KS_IP, KS_PORT);
+
+    // 3. Manejo de errores en la conexión
+    if (fd_ks == -1) {
+        log_error(logger, "No se pudo conectar a Kernel Scheduler en %s:%s",
+                  ip, puerto);
+        return EXIT_FAILURE;
+    }
+
+    // Realizar handshake (con MSG_HANDSHAKE_IO)
+    // 1. Enviar saludo
+    op_code saludo = MSG_HANDSHAKE_IO;
+    enviar_mensaje(fd_ks, &saludo, sizeof(op_code));
+
+    // 2. Esperar OK 
+    int size_resp;
+    op_code* respuesta = recibir_mensaje(fd_ks, &size_resp);
+    
+    if (*respuesta == MSG_OK) {
+        // Log obligatorio: log_info(logger, "## Conectado a Kernel Scheduler");
+        log_info(logger, "## Conectado a Kernel Scheduler y aceptado");
+    }
+    free(respuesta); // Liberar la memoria del mensaje recibido
  
-    // TODO Bianca: conectarse a Kernel Scheduler
-    // int fd_ks = crear_conexion(KS_IP, KS_PORT);
-    // enviar handshake MSG_HANDSHAKE_IO
-    // Log obligatorio: log_info(logger, "## Conectado a Kernel Scheduler");
- 
-    // TODO Bianca: quedarse esperando peticiones del KS
+    // Quedarse esperando peticiones del KS
+    pause();
     // pause(); por ahora alcanza para CP1
- 
+    
     // TODO CP2: implementar STDIN, STDOUT y SLEEP
+
+    // Eliminar el config y el log para liberar la memoria
+    config_destroy(config);
+    log_destroy(logger);
+    return EXIT_SUCCESS;
 }
     
