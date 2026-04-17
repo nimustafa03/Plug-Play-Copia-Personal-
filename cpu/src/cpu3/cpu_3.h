5e0a1a26fc1e728c7d5939ea17edeff2e6d085b2
Adriel <adouton@frba.utn.edu.ar>
2026-04-17 19:06:55 +0000

    Subo estructura CPUs

---
+++ cpu/src/cpu3/cpu_3.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_CPU_3_H
+#define UTILS_CPU_3_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include "/home/utnso/Desktop/tp-2026-1c-Grupo3113/utils/src/utils/conexiones.h"
+#include "/home/utnso/Desktop/tp-2026-1c-Grupo3113/utils/src/utils/mensajes.h"
+
+t_config* iniciar_config(void);
+t_log* iniciar_logger(void);
+
+#endif // UTILS_CPU_3_H
