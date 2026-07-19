bc8c9c938628fc6ed4886822cd67c65f4e2ba0be
Nicolas <nimustafa03@gmail.com>
2026-07-19 02:39:12 +0000

    hola

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -141,6 +141,20 @@ int main(int argc, char* argv[]) {
         int size;
         t_contexto * contexto =  malloc(sizeof(t_contexto));
         contexto = recibir_mensaje(fd_km, &size);         // KM envia contexto
+
+        printf("AX %d",contexto->registros.ax);
+        printf("BX %d",contexto->registros.bx);
+        printf("CX %d",contexto->registros.cx);
+        printf("DI %d",contexto->registros.di);
+        printf("DX %d",contexto->registros.dx);
+        printf("EAX %d",contexto->registros.eax);
+        printf("EBX %d",contexto->registros.ebx);
+        printf("ECX %d",contexto->registros.ecx);
+        printf("EDX %d",contexto->registros.edx);
+        printf("PC %d",contexto->registros.pc);
+        printf("SI %d",contexto->registros.si);
+
+
         if (contexto == NULL) {
             log_info(logger_cpu, "Error al recibir contexto");
             break;
