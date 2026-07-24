704099706355e83769da7b088a4d450e4fe57bb3
Adriel <adouton@frba.utn.edu.ar>
2026-07-23 21:44:19 -0300

    /

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -200,7 +200,7 @@ int main(int argc, char* argv[]) {
                 log_info(logger_cpu, "Error en FETCH");
                 exit(EXIT_FAILURE);
             }
-
+            log_info(logger_cpu, "recibiendo segmentos");
             actualizar_tabla_segmentos(contexto,fd_km,logger_cpu);
 
             // DECODE
